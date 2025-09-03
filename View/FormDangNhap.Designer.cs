5f34183d6d9c463d24f8edddebe53795c8d97491
BackDevEnd <ngoquan094@gmail.com>
2025-09-03 15:24:01 +0700

    đăng nhập

--- View/FormDangNhap.Designer.cs
+++ View/FormDangNhap.Designer.cs
@@ -41,6 +41,8 @@
             this.MatKhau = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
             this.Thoat = new Guna.UI2.WinForms.Guna2Button();
+            this.linkQuenMK = new System.Windows.Forms.LinkLabel();
+            this.radioMK = new System.Windows.Forms.RadioButton();
             this.guna2Panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
@@ -49,14 +51,17 @@
             // guna2Panel1
             // 
             this.guna2Panel1.BackColor = System.Drawing.Color.White;
+            this.guna2Panel1.Controls.Add(this.radioMK);
+            this.guna2Panel1.Controls.Add(this.linkQuenMK);
             this.guna2Panel1.Controls.Add(this.DangNhap);
             this.guna2Panel1.Controls.Add(this.tableLayoutPanel1);
             this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
             this.guna2Panel1.Controls.Add(this.Thoat);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(1838, 752);
+            this.guna2Panel1.Size = new System.Drawing.Size(1634, 602);
             this.guna2Panel1.TabIndex = 1;
             // 
             // DangNhap
@@ -67,17 +72,18 @@
             this.DangNhap.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.DangNhap.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DangNhap.ForeColor = System.Drawing.Color.White;
-            this.DangNhap.Location = new System.Drawing.Point(1249, 670);
+            this.DangNhap.Location = new System.Drawing.Point(1110, 536);
+            this.DangNhap.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DangNhap.Name = "DangNhap";
-            this.DangNhap.Size = new System.Drawing.Size(180, 54);
+            this.DangNhap.Size = new System.Drawing.Size(160, 43);
             this.DangNhap.TabIndex = 3;
             this.DangNhap.Text = "Đăng nhập";
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 380F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 752F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 338F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 668F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.ewdfdsfdsf, 0, 2);
@@ -85,15 +91,16 @@
             this.tableLayoutPanel1.Controls.Add(this.TenDangNhap, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.MaNV, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.MatKhau, 1, 3);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(706, 61);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(628, 49);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 136F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 116F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 116F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1132, 485);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 109F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 6F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1006, 388);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // label1
@@ -104,7 +111,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1126, 136);
+            this.label1.Size = new System.Drawing.Size(1000, 109);
             this.label1.TabIndex = 0;
             this.label1.Text = "Đăng Nhập";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -114,9 +121,9 @@
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Right;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(54, 136);
+            this.label2.Location = new System.Drawing.Point(56, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(323, 116);
+            this.label2.Size = new System.Drawing.Size(279, 93);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tên đăng nhập: ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -126,9 +133,9 @@
             this.ewdfdsfdsf.AutoSize = true;
             this.ewdfdsfdsf.Dock = System.Windows.Forms.DockStyle.Right;
             this.ewdfdsfdsf.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ewdfdsfdsf.Location = new System.Drawing.Point(84, 252);
+            this.ewdfdsfdsf.Location = new System.Drawing.Point(82, 202);
             this.ewdfdsfdsf.Name = "ewdfdsfdsf";
-            this.ewdfdsfdsf.Size = new System.Drawing.Size(293, 116);
+            this.ewdfdsfdsf.Size = new System.Drawing.Size(253, 93);
             this.ewdfdsfdsf.TabIndex = 2;
             this.ewdfdsfdsf.Text = "Mã nhân viên: ";
             this.ewdfdsfdsf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -138,9 +145,9 @@
             this.ffsfsfsdf.AutoSize = true;
             this.ffsfsfsdf.Dock = System.Windows.Forms.DockStyle.Right;
             this.ffsfsfsdf.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ffsfsfsdf.Location = new System.Drawing.Point(162, 368);
+            this.ffsfsfsdf.Location = new System.Drawing.Point(151, 295);
             this.ffsfsfsdf.Name = "ffsfsfsdf";
-            this.ffsfsfsdf.Size = new System.Drawing.Size(215, 117);
+            this.ffsfsfsdf.Size = new System.Drawing.Size(184, 93);
             this.ffsfsfsdf.TabIndex = 3;
             this.ffsfsfsdf.Text = "Mật khẩu: ";
             this.ffsfsfsdf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -157,12 +164,12 @@
             this.TenDangNhap.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.TenDangNhap.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.TenDangNhap.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.TenDangNhap.Location = new System.Drawing.Point(384, 141);
-            this.TenDangNhap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TenDangNhap.Location = new System.Drawing.Point(342, 113);
+            this.TenDangNhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TenDangNhap.Name = "TenDangNhap";
             this.TenDangNhap.PlaceholderText = "";
             this.TenDangNhap.SelectedText = "";
-            this.TenDangNhap.Size = new System.Drawing.Size(744, 106);
+            this.TenDangNhap.Size = new System.Drawing.Size(660, 85);
             this.TenDangNhap.TabIndex = 4;
             // 
             // MaNV
@@ -176,12 +183,12 @@
             this.MaNV.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.MaNV.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.MaNV.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.MaNV.Location = new System.Drawing.Point(384, 257);
-            this.MaNV.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaNV.Location = new System.Drawing.Point(342, 206);
+            this.MaNV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaNV.Name = "MaNV";
             this.MaNV.PlaceholderText = "";
             this.MaNV.SelectedText = "";
-            this.MaNV.Size = new System.Drawing.Size(744, 106);
+            this.MaNV.Size = new System.Drawing.Size(660, 85);
             this.MaNV.TabIndex = 5;
             // 
             // MatKhau
@@ -195,21 +202,22 @@
             this.MatKhau.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.MatKhau.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.MatKhau.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.MatKhau.Location = new System.Drawing.Point(384, 373);
-            this.MatKhau.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MatKhau.Location = new System.Drawing.Point(342, 299);
+            this.MatKhau.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MatKhau.Name = "MatKhau";
             this.MatKhau.PlaceholderText = "";
             this.MatKhau.SelectedText = "";
-            this.MatKhau.Size = new System.Drawing.Size(744, 107);
+            this.MatKhau.Size = new System.Drawing.Size(660, 85);
             this.MatKhau.TabIndex = 6;
             // 
             // guna2PictureBox1
             // 
             this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
             this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 54);
+            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 43);
+            this.guna2PictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2PictureBox1.Name = "guna2PictureBox1";
-            this.guna2PictureBox1.Size = new System.Drawing.Size(700, 670);
+            this.guna2PictureBox1.Size = new System.Drawing.Size(622, 536);
             this.guna2PictureBox1.TabIndex = 1;
             this.guna2PictureBox1.TabStop = false;
             // 
@@ -225,24 +233,50 @@
             this.Thoat.Image = ((System.Drawing.Image)(resources.GetObject("Thoat.Image")));
             this.Thoat.ImageSize = new System.Drawing.Size(45, 46);
             this.Thoat.Location = new System.Drawing.Point(0, 0);
+            this.Thoat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Thoat.Name = "Thoat";
-            this.Thoat.Size = new System.Drawing.Size(46, 45);
+            this.Thoat.Size = new System.Drawing.Size(41, 36);
             this.Thoat.TabIndex = 0;
             this.Thoat.Click += new System.EventHandler(this.guna2Button1_Click);
             // 
+            // linkQuenMK
+            // 
+            this.linkQuenMK.AutoSize = true;
+            this.linkQuenMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkQuenMK.Location = new System.Drawing.Point(1406, 466);
+            this.linkQuenMK.Name = "linkQuenMK";
+            this.linkQuenMK.Size = new System.Drawing.Size(146, 25);
+            this.linkQuenMK.TabIndex = 4;
+            this.linkQuenMK.TabStop = true;
+            this.linkQuenMK.Text = "Quên mật khẩu";
+            // 
+            // radioMK
+            // 
+            this.radioMK.AutoSize = true;
+            this.radioMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMK.Location = new System.Drawing.Point(970, 466);
+            this.radioMK.Name = "radioMK";
+            this.radioMK.Size = new System.Drawing.Size(158, 29);
+            this.radioMK.TabIndex = 5;
+            this.radioMK.TabStop = true;
+            this.radioMK.Text = "Hiện mật khẩu";
+            this.radioMK.UseVisualStyleBackColor = true;
+            // 
             // FormDangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1838, 752);
+            this.ClientSize = new System.Drawing.Size(1634, 602);
             this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormDangNhap";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormDangNhap";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
@@ -264,6 +298,8 @@
         private Guna.UI2.WinForms.Guna2TextBox TenDangNhap;
         private Guna.UI2.WinForms.Guna2TextBox MaNV;
         private Guna.UI2.WinForms.Guna2TextBox MatKhau;
+        private System.Windows.Forms.RadioButton radioMK;
+        private System.Windows.Forms.LinkLabel linkQuenMK;
     }
 }
 
