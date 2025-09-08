ae0c24218b5ae08749a035f2cda71c34a7edbe71
BackDevEnd <ngoquan094@gmail.com>
2025-09-08 19:04:27 +0700

    Add files via upload
    controller thong ke

---
+++ Controller/Controller_TKPB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY_NHAN_VIEN.Controller
+{
+    class Controller_TKPB
+    {
+        public static System.Data.DataTable GetData(string TenPhongBan, string GioiTinh)
+        {
+            return Model.Model_TKPB.GetData(TenPhongBan, GioiTinh);
+        }
+        public System.Data.DataTable GetGioiTinh()
+        {
+            return Model.Model_TKPB.GetGioiTinh();
+        }
+        public static System.Data.DataTable GetGenderCountByDepartment(string TenPhong)
+        {
+            return Model.Model_TKPB.GetGenderCountByDepartment(TenPhong);
+        }
+    }
+}
---
+++ Controller/Controller_UC_TKNV.cs
@@ -0,0 +1,33 @@
+﻿using QUAN_LY_NHAN_VIEN.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY_NHAN_VIEN.Controller
+{
+    class Controller_UC_TKNV
+    {
+        public static DataTable GetThongKeNV(string PhongBan, string ChucVu, string GioiTinh, string LoaiHD, DateTime? NgayBatDau, DateTime? NgayKetThuc, string TrangThai)
+        {
+            return Model.Model_UC_TKNV.GetNhanVien(PhongBan, ChucVu, GioiTinh, LoaiHD, NgayBatDau, NgayKetThuc, TrangThai);
+        }
+        public DataTable LayPhongBan()
+        {
+            return Model_UC_TKNV.GetPhongBan(); // gọi thẳng từ Model
+        }
+
+        public DataTable LayChucVu()
+        {
+            return Model_UC_TKNV.GetChucVu(); // gọi thẳng từ Model
+        }
+
+        public DataTable LayLoaiHD()
+        {
+            return Model_UC_TKNV.GetLoaiHD(); // gọi thẳng từ Model
+        }
+    }
+}
